07dd905c1f12d1553dcdb0b4a659f977e2df068d
Jacques du Plessis <dupjac3@gmail.com>
2024-09-08 17:39:00 +0200

    Initial commit

---
+++ Prog Web Application/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Get the video element
+const video = document.getElementById("background-video");
+
+// Set the time (in seconds) when the video should stop
+const stopTime = 10; // For example, stop after 30 seconds
+
+// Function to stop the video
+function stopVideo() {
+    video.pause(); // Pause the video
+}
+
+// Set a timeout to stop the video after the specified time
+setTimeout(stopVideo, stopTime * 1000); // Convert seconds to milliseconds
